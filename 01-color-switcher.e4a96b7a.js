c4622318057ca098d5237aabd5b7d55a722828a0
DmytroKarpenko88 <88401783+DmytroKarpenko88@users.noreply.github.com>
2023-02-19 22:21:22 +0000

    Deploying to gh-pages from @ DmytroKarpenko88/goit-js-hw-09@ee823c244c5fa787ff30506ea88d9d553490d692 🚀

--- 01-color-switcher.e4a96b7a.js
+++
@@ -1,2 +0,0 @@
-[document.querySelector("body"),document.querySelector("button[data-start]"),document.querySelector("button[data-stop]")][1].addEventListener("click",(function(t){t.currentTarget.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.e4a96b7a.js.map
