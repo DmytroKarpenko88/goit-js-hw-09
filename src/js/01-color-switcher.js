bc0176e162a324ba6be9362988336bee37e1ac37
Dmytro Karpenko <kardmitriy@gmail.com>
2023-02-19 23:09:08 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,11 @@
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+};
 
+refs.btnStart.addEventListener('click', startBtnGenerator);
+
+function startBtnGenerator(event) {
+  event.currentTarget.disabled = true;
+}
