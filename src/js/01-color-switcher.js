2788487cae3251c2c7139309b1bd221986a16e83
Dmytro Karpenko <kardmitriy@gmail.com>
2023-02-20 00:20:55 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -4,8 +4,28 @@ const refs = {
   btnStop: document.querySelector('button[data-stop]'),
 };
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
 refs.btnStart.addEventListener('click', startBtnGenerator);
+
+function bodyColorChange() {
+  refs.body.style.backgroundColor = getRandomHexColor();
+}
 
 function startBtnGenerator(event) {
   event.currentTarget.disabled = true;
+  // refs.body.style.backgroundColor = getRandomHexColor();
+
+  timerId = setInterval(bodyColorChange, 1000);
+}
+
+refs.btnStop.addEventListener('click', stopBtnGenerator);
+
+function stopBtnGenerator() {
+  clearInterval(timerId);
+  refs.btnStart.disabled = false;
 }
